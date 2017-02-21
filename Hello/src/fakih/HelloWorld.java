92e5f1829748ecfaba23e53d2a87487270a75992
Omar Fakih <323917138@C-4JNJVR1.tdsb.on.ca>
2017-02-21 14:48:00 -0500

    Pushing my first program, Hello World

---
+++ Hello/src/fakih/HelloWorld.java
@@ -0,0 +1,17 @@
+package fakih;
+/** 
+ *HelloWorld.java
+ *Outputs HelloWorld to console
+ *February 21st, 2017
+ *@author Omar Fakih
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World");
+		
+
+	}
+
+}
